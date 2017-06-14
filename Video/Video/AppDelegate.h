69d92c46f813bc22f3c6a698bb7a0eabf17eb07a
Rostyslav Stepanyak <rostyslav.stepanyak@darkmatter.ae>
2017-06-14 20:52:25 +0400

    setup CTVideo as raw binaries

---
+++ Video/Video/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Video
+//
+//  Created by Rostyslav Stepanyak on 6/14/17.
+//  Copyright © 2017 Rostyslav.Stepanyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Video/Video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Video
+//
+//  Created by Rostyslav Stepanyak on 6/14/17.
+//  Copyright © 2017 Rostyslav.Stepanyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
